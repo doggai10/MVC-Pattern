a00509a221162c2a199e7e569daf37badd02706b
doggai10 <doggai77@gmail.com>
2021-11-22 00:57:12 +0900

    상품 입력 검증 로직 추가

--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -76,11 +77,42 @@ public class FormItemController {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes){
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model){
         log.info("item.open={}", item.getOpen());
         log.info("item.regions={}", item.getRegions());
         log.info("item.itemType={}", item.getItemType());
         log.info("item.deliveryCode={}", item.getDeliveryCode());
+
+        //검증 오류 결과를 보관
+        Map<String, String> errors = new HashMap<>();
+
+        //검증 로직
+        if(!StringUtils.hasText(item.getItemName())){
+            errors.put("itemName", "상품 이름은 필수입니다.");
+        }
+        if(item.getPrice() == null || item.getPrice()<1000 || item.getPrice()>1000000){
+            errors.put("price","가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+        }
+        if(item.getQuantity() == null || item.getQuantity()>=9999){
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice() !=null && item.getQuantity() !=null){
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if(resultPrice < 10000){
+                errors.put("globalError", "가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재 값 = "+resultPrice);
+            }
+        }
+
+        //검증에 실패하면 다시 입력 폼으로
+        if(!errors.isEmpty()){
+            log.info("errors = {} ", errors);
+            model.addAttribute("errors", errors);
+            return "form/addForm";
+        }
+
+        // 성공 로직
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
